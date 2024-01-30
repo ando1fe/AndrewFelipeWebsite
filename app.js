2fc373b764ba3a49ea66f705c69d1aa416e3300d
ando1fe <47644248+ando1fe@users.noreply.github.com>
2024-01-30 01:45:14 -0500

    loading fix x3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let percentage = 0;
 let percentageElement = document.getElementById('percentage');
 
 let loadingInterval = setInterval(() => {
-    percentage++;
+    percentage += 5; // Increase the percentage by 5 instead of 1
     percentageElement.textContent = percentage + '%';
     loadingBar.style.width = percentage + '%';
 
@@ -23,9 +23,12 @@ let loadingInterval = setInterval(() => {
             loadingBar.style.opacity = '1'; // Reset the opacity of the loading bar
 
             introText.textContent = 'Hi there!';
+            introText.style.display = 'block'; // Show the intro text
+            introText.style.opacity = '0'; // Set the opacity of the intro text to 0
             setTimeout(function() {
+                introText.style.opacity = '1'; // Fade in the intro text
                 introText.textContent = 'Welcome to my resume! Scroll to learn more....';
             }, 2000);
-        }, 1000);
+        }, 2000); // Change the delay to 2000 milliseconds (2 seconds)
     }
-}, 25);
+}, 20); // Change the interval duration to 20 milliseconds

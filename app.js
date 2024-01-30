0af935d54fa1a550e84e17165ffc0a7d9101e7c1
ando1fe <47644248+ando1fe@users.noreply.github.com>
2024-01-30 03:27:04 -0500

    js and css fixes

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ let loadingBar = document.getElementById('loading-bar');
 let introText = document.getElementById('intro-text');
 let percentage = 0;
 let percentageElement = document.getElementById('percentage');
-let loadingText = document.getElementById('loading-text'); // New element for "Loading in..." text
-let arrow = document.getElementById('arrow'); // New element for the bouncing arrow
+let loadingText = document.getElementById('loading-text');
+let arrow = document.getElementById('arrow');
 
 let loadingInterval = setInterval(() => {
-    percentage += 2; // Increase the percentage by 1
+    percentage += 2;
     percentageElement.textContent = percentage + '%';
     loadingBar.style.width = percentage + '%';
 
@@ -16,42 +16,28 @@ let loadingInterval = setInterval(() => {
         clearInterval(loadingInterval);
 
         setTimeout(function () {
-            // Start the fade out effect
             loadingBar.classList.add('fade-out');
             loadingText.classList.add('fade-out');
-        }, 1000); // Adjust the delay as needed
+        }, 1000);
 
         setTimeout(function () {
             loadingBar.style.display = 'none';
             loadingText.style.display = 'none';
-
-            // Reset the opacity
-            loadingBar.style.opacity = '1';
-            loadingText.style.opacity = '1';
-
-            // Start the fade in effect for the intro text
             introText.textContent = 'Hi there!';
             introText.classList.add('fade-in');
-        }, 2000); // Delay for the 'Hi there!' text fade-in
+        }, 2000);
 
         setTimeout(function () {
-            // Fade out the 'Hi there!' text
             introText.classList.remove('fade-in');
             introText.classList.add('fade-out');
-        }, 4000); // Delay for the 'Hi there!' text fade-out
+        }, 4000);
 
         setTimeout(function () {
-            // Remove the 'Hi there!' text and fade-out class
-            introText.textContent = '';
-            introText.classList.remove('fade-out');
-
-            // Add the "Welcome to my resume!" text and fade it in
             introText.textContent = 'Welcome to my resume! Scroll to learn more....';
+            introText.classList.remove('fade-out');
             introText.classList.add('fade-in');
-
-            // Display and fade in the arrow at the same time
             arrow.style.display = 'block';
             arrow.classList.add('fade-in');
-        }, 6000); // Delay for the 'Welcome to my resume!' text and arrow
+        }, 6000);
     }
-}, 100); // Adjust the interval as needed
+}, 50);

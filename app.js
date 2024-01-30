1adbb52254ad3864b8672f79c079d353c8c0e416
ando1fe <47644248+ando1fe@users.noreply.github.com>
2024-01-30 03:22:10 -0500

    Fix Attempt

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ let loadingText = document.getElementById('loading-text'); // New element for "L
 let arrow = document.getElementById('arrow'); // New element for the bouncing arrow
 
 let loadingInterval = setInterval(() => {
-    percentage += 1; // Increase the percentage by 1
+    percentage += 2; // Increase the percentage by 1
     percentageElement.textContent = percentage + '%';
     loadingBar.style.width = percentage + '%';
 
     if (percentage >= 100) {
         clearInterval(loadingInterval);
-        // Start the fade out effect
-        loadingBar.classList.add('fade-out');
-        loadingText.classList.add('fade-out');
+
+        setTimeout(function () {
+            // Start the fade out effect
+            loadingBar.classList.add('fade-out');
+            loadingText.classList.add('fade-out');
+        }, 1000); // Adjust the delay as needed
 
         setTimeout(function () {
             loadingBar.style.display = 'none';
@@ -25,9 +28,7 @@ let loadingInterval = setInterval(() => {
             // Reset the opacity
             loadingBar.style.opacity = '1';
             loadingText.style.opacity = '1';
-        }, 1000); // Adjust the delay as needed
 
-        setTimeout(function () {
             // Start the fade in effect for the intro text
             introText.textContent = 'Hi there!';
             introText.classList.add('fade-in');

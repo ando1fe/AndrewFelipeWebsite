8595385c49ffaf1c14afd21db0da23cbe4446ac1
ando1fe <47644248+ando1fe@users.noreply.github.com>
2024-01-30 03:30:33 -0500

    js fix

--- app.js
+++ app.js
@@ -15,29 +15,25 @@ let loadingInterval = setInterval(() => {
     if (percentage >= 100) {
         clearInterval(loadingInterval);
 
-        setTimeout(function () {
-            loadingBar.classList.add('fade-out');
-            loadingText.classList.add('fade-out');
-        }, 1000);
+        loadingBar.style.opacity = '0';
+        loadingText.style.opacity = '0';
 
         setTimeout(function () {
             loadingBar.style.display = 'none';
             loadingText.style.display = 'none';
             introText.textContent = 'Hi there!';
-            introText.classList.add('fade-in');
+            introText.style.opacity = '1';
         }, 2000);
 
         setTimeout(function () {
-            introText.classList.remove('fade-in');
-            introText.classList.add('fade-out');
+            introText.style.opacity = '0';
         }, 4000);
 
         setTimeout(function () {
             introText.textContent = 'Welcome to my resume! Scroll to learn more....';
-            introText.classList.remove('fade-out');
-            introText.classList.add('fade-in');
+            introText.style.opacity = '1';
             arrow.style.display = 'block';
-            arrow.classList.add('fade-in');
+            arrow.style.opacity = '1';
         }, 6000);
     }
 }, 50);
